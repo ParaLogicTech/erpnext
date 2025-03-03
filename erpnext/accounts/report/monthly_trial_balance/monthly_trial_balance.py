cfa5b2e8b4852c372f4d15523e61790f987625fc
Saif Ur Rehman <saif@paralogic.io>
2025-03-03 15:53:22 +0500

    feat: Monthly Trial Balance

---
+++ erpnext/accounts/report/monthly_trial_balance/monthly_trial_balance.py
@@ -0,0 +1,225 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+import frappe, erpnext
+from frappe import _
+from frappe.utils import flt, getdate, cstr
+from erpnext.accounts.report.financial_statements \
+	import filter_accounts, set_gl_entries_by_account, filter_out_zero_value_rows
+from erpnext.accounts.report.trial_balance.trial_balance import validate_filters, get_rootwise_opening_balances
+from erpnext.accounts.report.financial_statements import get_period_list
+
+
+def execute(filters=None):
+	validate_filters(filters)
+
+	period_list = get_period_list(filters.fiscal_year, filters.fiscal_year, "Monthly", False, filters.company)
+
+	data = get_data(filters, period_list)
+	columns = get_columns(period_list)
+	return columns, data
+
+
+def get_data(filters, period_list):
+	accounts = frappe.db.sql("""
+		select name, account_number, parent_account, account_name, root_type, report_type, lft, rgt
+		from `tabAccount`
+		where company=%s order by lft
+	""", filters.company, as_dict=True)
+
+	if not accounts:
+		return None
+
+	company_currency = erpnext.get_company_currency(filters.company)
+
+	accounts, accounts_by_name, parent_children_map = filter_accounts(accounts)
+
+	min_lft, max_rgt = frappe.db.sql("""
+		select min(lft), max(rgt)
+		from `tabAccount`
+		where company=%s
+	""", (filters.company,))[0]
+
+	gl_entries_by_account = {}
+
+	opening_balances = get_opening_balances(filters)
+
+	# add filter inside list so that the query in financial_statements.py doesn't break
+	if filters.project:
+		filters.project = [filters.project]
+
+	set_gl_entries_by_account(filters.company, filters.from_date, filters.to_date,
+		min_lft, max_rgt, filters, gl_entries_by_account,
+		ignore_closing_entries=not flt(filters.with_period_closing_entry))
+
+	total_row = calculate_values(accounts, gl_entries_by_account, opening_balances, filters, company_currency, period_list)
+	accumulate_values_into_parents(accounts, accounts_by_name, period_list)
+
+	data = prepare_data(accounts, filters, total_row, parent_children_map, company_currency, period_list)
+	data = filter_out_zero_value_rows(data, parent_children_map, show_zero_values=filters.get("show_zero_values"))
+
+	set_zero_for_group_accounts(data, parent_children_map, period_list)
+
+	return data
+
+
+def get_opening_balances(filters):
+	balance_sheet_opening = get_rootwise_opening_balances(filters, "Balance Sheet")
+	pl_opening = get_rootwise_opening_balances(filters, "Profit and Loss")
+
+	balance_sheet_opening.update(pl_opening)
+	return balance_sheet_opening
+
+
+def calculate_values(accounts, gl_entries_by_account, opening_balances, filters, company_currency, period_list):
+	init = {
+		"opening_balance": 0.0,
+		"debit": 0.0,
+		"credit": 0.0,
+		"closing_balance": 0.0,
+	}
+	for period in period_list:
+		init[period.key] = 0
+
+	total_row = {
+		"account": "'" + _("Total") + "'",
+		"account_name": "'" + _("Total") + "'",
+		"warn_if_negative": True,
+		"opening_balance": 0.0,
+		"debit": 0.0,
+		"credit": 0.0,
+		"closing_balance": 0.0,
+		"parent_account": None,
+		"indent": 0,
+		"has_value": True,
+		"currency": company_currency
+	}
+	for period in period_list:
+		total_row[period.key] = 0
+
+	for d in accounts:
+		d.update(init.copy())
+
+		# add opening
+		d["opening_balance"] = opening_balances.get(d.name, {}).get("opening_debit", 0) - opening_balances.get(d.name, {}).get("opening_credit", 0)
+
+		for entry in gl_entries_by_account.get(d.name, []):
+			if cstr(entry.is_opening) == "Yes":
+				continue
+
+			d["debit"] += flt(entry.debit)
+			d["credit"] += flt(entry.credit)
+
+			for period in period_list:
+				if period.from_date <= getdate(entry.posting_date) <= period.to_date:
+					d[period.key] += flt(entry.debit) - flt(entry.credit)
+
+		d["closing_balance"] = d["opening_balance"] + d["debit"] - d["credit"]
+
+		for field in get_value_fields(period_list):
+			total_row[field] += d[field]
+
+	return total_row
+
+
+def accumulate_values_into_parents(accounts, accounts_by_name, period_list):
+	for d in reversed(accounts):
+		if d.parent_account:
+			for key in get_value_fields(period_list):
+				accounts_by_name[d.parent_account][key] += d[key]
+
+
+def set_zero_for_group_accounts(data, parent_children_map, period_list):
+	for d in data:
+		if d.get('account') and parent_children_map.get(d['account']):
+			for key in get_value_fields(period_list):
+				del d[key]
+
+
+def prepare_data(accounts, filters, total_row, parent_children_map, company_currency, period_list):
+	data = []
+
+	for d in accounts:
+		has_value = False
+		row = {
+			"account": d.name,
+			"parent_account": d.parent_account,
+			"indent": d.indent,
+			"from_date": filters.from_date,
+			"to_date": filters.to_date,
+			"currency": company_currency,
+			"account_name": ('{} - {}'.format(d.account_number, d.account_name)
+				if d.account_number else d.account_name)
+		}
+
+		for key in get_value_fields(period_list):
+			row[key] = flt(d.get(key, 0.0), 3)
+
+			if abs(row[key]) >= 0.005:
+				# ignore zero values
+				has_value = True
+
+		row["has_value"] = has_value
+		data.append(row)
+
+	for key in get_value_fields(period_list):
+		total_row[key] = flt(total_row.get(key, 0.0), 3)
+
+	data.extend([{}, total_row])
+
+	return data
+
+
+def get_value_fields(period_list):
+	value_fields = ["opening_balance", "debit", "credit", "closing_balance"]
+	for period in period_list:
+		value_fields.append(period.key)
+
+	return value_fields
+
+
+def get_columns(period_list):
+	columns = [
+		{
+			"fieldname": "account",
+			"label": _("Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			"width": 300
+		},
+		{
+			"fieldname": "opening_balance",
+			"label": _("Opening Balance"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120
+		},
+	]
+
+	for period in period_list:
+		columns.append({
+			"fieldname": period.key,
+			"label": period.label,
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 110
+		})
+
+	columns += [
+		{
+			"fieldname": "closing_balance",
+			"label": _("Closing Balance"),
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 120
+		},
+		{
+			"fieldname": "currency",
+			"label": _("Currency"),
+			"fieldtype": "Link",
+			"options": "Currency",
+			"hidden": 1
+		},
+	]
+
+	return columns
