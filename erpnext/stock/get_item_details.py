ba5a714083775f4de6b78aa2332cf8f69a90cbe0
Saif Ur Rehman <saif@paralogic.io>
2025-03-17 03:11:21 +0500

    fix: send zero discount amount

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -275,6 +275,7 @@ def get_basic_details(args, item, overwrite_warehouse=True):
 		"net_rate": 0.0,
 		"net_amount": 0.0,
 		"discount_percentage": 0.0,
+		"discount_amount": 0.0,
 		"depreciation_percentage": get_depreciation_percentage(item, args),
 		"underinsurance_percentage": flt(args.get("default_underinsurance_percentage")),
 		"ignore_depreciation": get_ignore_depreciation(item),
