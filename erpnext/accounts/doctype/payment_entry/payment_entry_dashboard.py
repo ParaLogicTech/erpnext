4f6003dc6331c4205a09c797235597424ba92a7c
Saif Ur Rehman <saif@paralogic.io>
2025-05-23 12:46:53 +0500

    fix(POS Closing): make invoice/payment searchable

--- erpnext/accounts/doctype/payment_entry/payment_entry_dashboard.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry_dashboard.py
@@ -4,10 +4,17 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'reference_name',
+		'non_standard_fieldnames': {
+			'POS Closing Entry': 'document_name',
+		},
 		'transactions': [
 			{
 				'label': _('Refund Entry'),
 				'items': ['Payment Entry']
-			}
+			},
+			{
+				'label': _('POS Closing'),
+				'items': ['POS Closing Entry']
+			},
 		]
 	}
