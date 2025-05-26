0ff6c489a0e336b65aebc9614f068478c25a1754
Saif Ur Rehman <saif@paralogic.io>
2025-05-27 00:30:50 +0500

    fix: v15 upgrade patch fixes

--- erpnext/patches/v15_0/update_appointment_service_template.py
+++ erpnext/patches/v15_0/update_appointment_service_template.py
@@ -5,9 +5,13 @@ from frappe.utils.fixtures import sync_fixtures
 def execute():
     sync_fixtures(app="erpnext")
 
+    fieldname = "service_template"
+    if not frappe.db.has_column("Appointment", "service_template"):
+        fieldname = "project_template"
+
     appointments = frappe.get_all("Appointment", filters={
-        "service_template": ['is', 'set']
-    }, fields=["name", "service_template", "service_template_name"])
+        fieldname: ['is', 'set']
+    }, fields=["name", f"{fieldname} as service_template", f"{fieldname}_name as service_template_name"])
 
     for appt in appointments:
         if appt.service_template:
