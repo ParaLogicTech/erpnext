287487106dac3719bf0811d1deeee4e9659a5bd1
Saif Ur Rehman <saif@paralogic.io>
2025-08-18 13:46:40 +0500

    chore(Bank Deposit): rename child DocTypes

--- erpnext/accounts/doctype/bank_deposit_adjustment/bank_deposit_adjustment.py
+++ erpnext/accounts/doctype/bank_deposit_adjustment/bank_deposit_adjustment.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class DepositAdjustments(Document):
+class BankDepositAdjustment(Document):
 	pass
--- erpnext/accounts/doctype/bank_deposit_undeposited_entry/bank_deposit_undeposited_entry.py
+++ erpnext/accounts/doctype/bank_deposit_undeposited_entry/bank_deposit_undeposited_entry.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class UndepositedEntries(Document):
+class BankDepositUndepositedEntry(Document):
 	pass
