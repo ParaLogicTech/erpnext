6c01676c71763981791d4bb060a4990591377930
Saif Ur Rehman <saif@paralogic.io>
2025-09-04 16:50:40 +0500

    refactor: move FBR POS Integration to separate app

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -225,7 +225,6 @@ scheduler_events = {
 		"erpnext.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
 		"erpnext.support.doctype.issue.issue.set_service_level_agreement_variance",
 		"erpnext.selling.doctype.customer.customer.send_customer_birthday_notifications",
-		"erpnext.erpnext_integrations.fbr_pos_integration.post_fbr_pos_invoices_without_number",
 	],
 	"daily": [
 		"erpnext.stock.reorder_item.reorder_item",
