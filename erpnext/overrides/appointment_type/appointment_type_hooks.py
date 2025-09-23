c02afc99ad7f2f0f779d4a884acdd8bab485632e
Saif Ur Rehman <saif@paralogic.io>
2025-09-23 13:35:50 +0500

    feat(Appointment): get_appointment_timeslots_for_daterange API

--- erpnext/overrides/appointment_type/appointment_type_hooks.py
+++ erpnext/overrides/appointment_type/appointment_type_hooks.py
@@ -1,7 +1,7 @@
 # import frappe
 from frappe.utils import getdate
 from crm.crm.doctype.appointment_type.appointment_type import AppointmentType
-from erpnext.hr.doctype.holiday_list.holiday_list import get_default_holiday_list, is_holiday
+from erpnext.hr.doctype.holiday_list.holiday_list import get_default_holiday_list, is_holiday, get_holiday_dates_between
 
 
 class AppointmentTypeERP(AppointmentType):
@@ -12,6 +12,10 @@ class AppointmentTypeERP(AppointmentType):
 		date = getdate(date)
 		holiday_list = self.get_holiday_list()
 		return is_holiday(holiday_list, date)
+
+	def get_holidays(self, from_date, to_date):
+		holiday_list = self.get_holiday_list()
+		return get_holiday_dates_between(holiday_list, from_date, to_date)
 
 	def get_holiday_list(self):
 		if self.get("holiday_list"):
