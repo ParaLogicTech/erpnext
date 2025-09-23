5b1cc69cfad45f57c2f2c80ed5d38a2383069779
Saif Ur Rehman <saif@paralogic.io>
2025-09-24 01:09:49 +0500

    feat: get_vehicle_identifier util

--- erpnext/vehicles/utils.py
+++ erpnext/vehicles/utils.py
@@ -18,3 +18,20 @@ def format_vehicle_fields(doc):
 def format_vehicle_id(value):
 	import re
 	return re.sub(r"\s+", "", cstr(value).upper())
+
+
+def get_vehicle_identifier(vehicle):
+	if not vehicle:
+		return ""
+
+	vehicle_details = frappe.db.get_value("Vehicle", vehicle,
+		("license_plate", "chassis_no"), as_dict=1, cache=1)
+	if not vehicle_details:
+		return vehicle
+
+	if vehicle_details.license_plate:
+		return vehicle_details.license_plate
+	elif vehicle_details.chassis_no:
+		return vehicle_details.chassis_no
+	else:
+		return vehicle
