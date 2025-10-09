a4441f9adebf9d4d6e5c4a5a921b78104af74c56
Saif Ur Rehman <saif@paralogic.io>
2025-10-09 17:00:46 +0500

    fix: patch fix

--- erpnext/patches/v15_0/move_apply_taxes_on_advance_payment_accounts_settings.py
+++ erpnext/patches/v15_0/move_apply_taxes_on_advance_payment_accounts_settings.py
@@ -3,6 +3,9 @@ from frappe.utils import cint
 
 
 def execute():
+	if not frappe.get_meta("Projects Settings").has_field("apply_taxes_on_advance_payment"):
+		return
+
 	apply_taxes_on_advance_payment = cint(frappe.db.get_single_value("Projects Settings", "apply_taxes_on_advance_payment"))
 
 	frappe.reload_doctype("Accounts Settings")
