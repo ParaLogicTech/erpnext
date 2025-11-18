ad53a861060760650a3f5d455f92fbd34478325a
kunhimohamed <kunhimohamed6@gmail.com>
2025-11-18 16:53:39 +0400

    fix(asset): add fields in asset document and backend validation for supplier

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -52,6 +52,8 @@ class Asset(AccountsController):
 			reference_doc = frappe.get_doc(reference_doc, reference_name)
 			if reference_doc.get('company') != self.company:
 				frappe.throw(_("Company of asset {0} and purchase document {1} doesn't matches.").format(self.name, reference_doc.get('name')))
+			if reference_doc.get("supplier") != self.purchase_vendor:
+				frappe.throw(_("Purchase Vendor of asset {0} and purchase document {1} doesn't matches.").format(self.name, reference_doc.get('name')))
 
 
 		if self.is_existing_asset and self.purchase_invoice:
