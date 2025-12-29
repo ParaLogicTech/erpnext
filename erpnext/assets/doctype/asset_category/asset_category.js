fb0a92b65f9e0dcf451ad0104762cafca1bbf27f
kunhimohamed <kunhimohamed6@gmail.com>
2025-12-29 11:27:02 +0400

    fix(asset category): asset category group introduction in in asset category

--- erpnext/assets/doctype/asset_category/asset_category.js
+++ erpnext/assets/doctype/asset_category/asset_category.js
@@ -2,9 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Category', {
-	setup: function(frm) {
-		frm.set_query('asset_category_group', {'name': ['!=', __("All Asset Category Group")]});
-	},
 	onload: function(frm) {
 		frm.add_fetch('company_name', 'accumulated_depreciation_account', 'accumulated_depreciation_account');
 		frm.add_fetch('company_name', 'depreciation_expense_account', 'accumulated_depreciation_account');
