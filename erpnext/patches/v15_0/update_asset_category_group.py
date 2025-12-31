928fee0650d9de21d68177b430984ab79653f3ee
kunhimohamed <kunhimohamed6@gmail.com>
2025-12-31 14:34:53 +0400

    fix(asset): include backtick for space contain tables in sql

--- erpnext/patches/v15_0/update_asset_category_group.py
+++ erpnext/patches/v15_0/update_asset_category_group.py
@@ -5,12 +5,10 @@ def execute():
     Update asset.asset_category_group
     by fetching it from asset_category.asset_category_group
     """
-
-    frappe.db.sql("""
-        UPDATE `tabAsset` a
-        INNER JOIN `tabAsset Category` ac
-            ON ac.name = a.asset_category
-        SET a.asset_category_group = ac.asset_category_group
-        WHERE
-            ((a.asset_category IS NOT NULL) AND (ac.asset_category_group IS NOT NULL));
-    """)
+    frappe.db.sql("""UPDATE tabAsset a 
+        INNER JOIN `tabAsset Category` ac 
+            ON ac.name = a.asset_category 
+        SET a.asset_category_group = ac.asset_category_group 
+        WHERE 
+            (((a.asset_category IS NOT NULL) AND (a.asset_category != '')) AND 
+            ((ac.asset_category_group IS NOT NULL) AND (ac.asset_category_group != '')))""")
