335d623cc02513bf67a50d01f7c7a4c2b037ee96
kunhimohamed <kunhimohamed6@gmail.com>
2026-04-11 19:03:00 +0400

    feat: upgrade rename tool

--- erpnext/utilities/doctype/rename_tool/rename_tool.js
+++ erpnext/utilities/doctype/rename_tool/rename_tool.js
@@ -1,32 +1,45 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-
 frappe.ui.form.on("Rename Tool", {
-	onload: function(frm) {
-		return frappe.call({
-			method: "erpnext.utilities.doctype.rename_tool.rename_tool.get_doctypes",
-			callback: function(r) {
-				frm.set_df_property("select_doctype", "options", r.message);
-			}
-		});
-	},
-	refresh: function(frm) {
+	refresh: function (frm) {
 		frm.disable_save();
-		if (!frm.doc.file_to_rename) {
-			frm.get_field("rename_log").$wrapper.html("");
-		}
-		frm.page.set_primary_action(__("Rename"), function() {
-			frm.get_field("rename_log").$wrapper.html("<p>Renaming...</p>");
+
+		frm.get_field("file_to_rename").df.options = {
+			restrictions: {
+				allowed_file_types: [".csv"],
+			},
+		};
+
+		frm.page.set_primary_action(__("Rename"), function () {
 			frappe.call({
 				method: "erpnext.utilities.doctype.rename_tool.rename_tool.upload",
 				args: {
-					select_doctype: frm.doc.select_doctype
+					select_doctype: frm.doc.select_doctype,
 				},
-				callback: function(r) {
-					frm.get_field("rename_log").$wrapper.html(r.message.join("<br>"));
-				}
+				freeze: true,
+				freeze_message: __("Scheduling..."),
+				callback: function () {
+					frappe.msgprint({
+						message: __("Rename jobs for doctype {0} have been enqueued.", [
+							frm.doc.select_doctype,
+						]),
+						alert: true,
+						indicator: "green",
+					});
+					frm.set_value("select_doctype", "");
+					frm.set_value("file_to_rename", "");
+				},
+				error: function (r) {
+					frappe.msgprint({
+						message: __("Rename jobs for doctype {0} have not been enqueued.", [
+							frm.doc.select_doctype,
+						]),
+						alert: true,
+						indicator: "red",
+					});
+				},
 			});
 		});
 	}
-})
+});
