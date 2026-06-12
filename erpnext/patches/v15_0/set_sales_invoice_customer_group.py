124566d2817a6a574211d24c1f9fc43eeaf09396
Saif Ur Rehman <saif@paralogic.io>
2026-06-12 16:18:20 +0500

    feat: update reset_transaction_customer_group patch to include SO and QTN

--- erpnext/patches/v15_0/set_sales_invoice_customer_group.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def execute():
-	frappe.db.sql("""
-		update `tabSales Invoice` si
-		inner join `tabCustomer` c on c.name = si.bill_to
-		set si.customer_group = c.customer_group
-	""")
