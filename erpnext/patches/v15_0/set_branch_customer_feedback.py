1a00fd662689c6d542339033336590adc5246ccd
kunhimohamed <kunhimohamed6@gmail.com>
2026-06-19 11:48:20 +0400

    fix(CFB Branch Update): add patch

--- erpnext/patches/v15_0/set_branch_customer_feedback.py
+++ erpnext/patches/v15_0/set_branch_customer_feedback.py
@@ -3,30 +3,36 @@ import click
 
 
 def execute():
-    for cf in frappe.get_all(
+    feedbacks = frappe.get_all(
         "Customer Feedback",
         fields=["name", "project", "reference_doctype", "reference_name"],
-    ):
-        branch = None
+    )
 
-        if cf.project:
-            branch = frappe.get_cached_value("Project", cf.project, "branch")
-        elif (
-            cf.reference_doctype
-            and cf.reference_name
-            and frappe.get_meta(cf.reference_doctype).has_field("branch")
-        ):
-            branch = frappe.get_cached_value(
-                cf.reference_doctype,
-                cf.reference_name,
-                "branch",
-            )
+    with click.progressbar(feedbacks, label="Updating Branches") as bar:
+        for cf in bar:
+            branch = None
 
-        if branch:
-            frappe.db.set_value(
-                "Customer Feedback",
-                cf.name,
-                "branch",
-                branch,
-                update_modified=False,
-            )
+            if cf.project:
+                branch = frappe.get_cached_value("Project", cf.project, "branch")
+
+            elif (
+                cf.reference_doctype
+                and cf.reference_name
+                and frappe.get_meta(cf.reference_doctype).has_field("branch")
+            ):
+                branch = frappe.get_cached_value(
+                    cf.reference_doctype,
+                    cf.reference_name,
+                    "branch",
+                )
+
+            if branch:
+                frappe.db.set_value(
+                    "Customer Feedback",
+                    cf.name,
+                    "branch",
+                    branch,
+                    update_modified=False,
+                )
+
+    click.secho("CFB Branch update completed.", fg="green")
