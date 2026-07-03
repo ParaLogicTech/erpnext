55362ac551abe4b9e46bcaa61b7c638d113cceb5
Saif Ur Rehman <saif@paralogic.io>
2026-07-03 13:47:19 +0500

    perf: 2 queries instead of loop and update patch

--- erpnext/patches/v15_0/set_branch_customer_feedback.py
+++ erpnext/patches/v15_0/set_branch_customer_feedback.py
@@ -1,38 +1,25 @@
 import frappe
 import click
 
+from frappe.utils.fixtures import sync_fixtures
+
 
 def execute():
-    feedbacks = frappe.get_all(
-        "Customer Feedback",
-        fields=["name", "project", "reference_doctype", "reference_name"],
-    )
+    sync_fixtures(app="erpnext")
 
-    with click.progressbar(feedbacks, label="Updating Branches") as bar:
-        for cf in bar:
-            branch = None
+    frappe.db.sql("""
+        update `tabCustomer Feedback` cfb
+        inner join `tabProject` p on p.name = cfb.project
+        set cfb.branch = p.branch
+    """)
 
-            if cf.project:
-                branch = frappe.get_cached_value("Project", cf.project, "branch")
+    reference_doctypes = frappe.db.sql_list("select distinct reference_doctype from `tabCustomer Feedback`")
+    for doctype in reference_doctypes:
+        if not doctype or not frappe.get_meta(doctype).has_field("branch"):
+            continue
 
-            elif (
-                cf.reference_doctype
-                and cf.reference_name
-                and frappe.get_meta(cf.reference_doctype).has_field("branch")
-            ):
-                branch = frappe.get_cached_value(
-                    cf.reference_doctype,
-                    cf.reference_name,
-                    "branch",
-                )
-
-            if branch:
-                frappe.db.set_value(
-                    "Customer Feedback",
-                    cf.name,
-                    "branch",
-                    branch,
-                    update_modified=False,
-                )
-
-    click.secho("CFB Branch update completed.", fg="green")
+        frappe.db.sql(f"""
+            update `tabCustomer Feedback` cfb
+            inner join `tab{doctype}` ref on ref.name = cfb.reference_name and cfb.reference_doctype = '{doctype}'
+            set cfb.branch = ref.branch
+        """)
