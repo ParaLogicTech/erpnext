0c4d1c4589cc7fa55133bf597d157f21b8e5e659
kunhimohamed <kunhimohamed6@gmail.com>
2026-07-10 17:16:09 +0400

    feat[Maintenance Schedule]: add new scheduler method

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -264,6 +264,7 @@ scheduler_events = {
 		"erpnext.support.doctype.service_level_agreement.service_level_agreement.check_agreement_status",
 		"erpnext.selling.doctype.quotation.quotation.set_expired_status",
 		"erpnext.maintenance.doctype.maintenance_schedule.maintenance_schedule.create_opportunity_from_schedule",
+        "erpnext.maintenance.doctype.maintenance_schedule.maintenance_schedule.send_maintenance_due_reminder",
 		"erpnext.projects.doctype.service_warranty.service_warranty.set_warranty_service_expired",
 	],
 	"daily_long": [
