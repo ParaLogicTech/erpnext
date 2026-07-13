a013b851ff2ab3494de5a9dbdd285902d41a55cc
kunhimohamed <kunhimohamed6@gmail.com>
2026-07-13 12:05:18 +0400

    feat(Maintenance Schedule): feature to stop the reminder at msd level and msd detail level

--- erpnext/maintenance/doctype/maintenance_schedule/maintenance_schedule.py
+++ erpnext/maintenance/doctype/maintenance_schedule/maintenance_schedule.py
@@ -66,7 +66,7 @@ class MaintenanceSchedule(TransactionBase):
 		return adjust_date_for_holidays(scheduled_date, holiday_list)
 	
 	def send_maintenance_due_reminder_in_advance(self, schedule):
-		if not schedule.disable_the_advance_reminder:
+		if not schedule.disable_reminder:
 			self.run_method("notify_maintenance_remainder_in_advance", schedule)
 
 	def send_maintenance_schedule_reminder_notification(self, row_name):
@@ -330,7 +330,7 @@ def send_maintenance_due_reminder_in_advance(for_date=None):
 
 	for schedule in schedule_data:
 		msd_doc = frappe.get_doc(schedule.parenttype, schedule.parent)
-		if not msd_doc.disable_the_advance_reminder:
+		if not msd_doc.disable_reminder:
 			msd_doc.run_method("send_maintenance_due_reminder_in_advance", schedule)
 
 
