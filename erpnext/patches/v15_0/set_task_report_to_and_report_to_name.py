2cc6a4c719958096c7fbe8a2d0e0150ac0f2f2d8
kunhimohamed <kunhimohamed6@gmail.com>
2026-08-26 09:44:14 +0400

    fix: update reports_to if its joining date is less than the task date

--- erpnext/patches/v15_0/set_task_report_to_and_report_to_name.py
+++ erpnext/patches/v15_0/set_task_report_to_and_report_to_name.py
@@ -10,10 +10,12 @@ def execute():
 	frappe.db.sql("""
 		update `tabTask` t
 		inner join `tabEmployee` emp on emp.name = t.assigned_to
+		inner join `tabEmployee` reports_to_emp on reports_to_emp.name = emp.reports_to
 		set
 			t.reports_to = emp.reports_to,
 			t.reports_to_name = emp.reports_to_name
 		where
 			ifnull(t.assigned_to, '') != ''
 			and ifnull(emp.reports_to, '') != ''
+			and reports_to_emp.date_of_joining <= t.exp_start_date
 	""")
