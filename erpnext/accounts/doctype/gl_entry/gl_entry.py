0147dd1ec5c511defdfbfb386e71fbafc5db689b
kunhimohamed <kunhimohamed6@gmail.com>
2026-09-21 18:49:09 +0400

    fix[Gl Entry, Period Closing Voucher]: changes the autoname and hash length of GL Entry, changed the timout for Period closing voucher

--- erpnext/accounts/doctype/gl_entry/gl_entry.py
+++ erpnext/accounts/doctype/gl_entry/gl_entry.py
@@ -25,7 +25,7 @@ class GLEntry(Document):
 		Temporarily name doc for fast insertion
 		name will be changed using autoname options (in a scheduled job)
 		"""
-		self.name = frappe.generate_hash(txt="", length=10)
+		self.name = frappe.generate_hash(txt="", length=15)
 
 	def validate(self):
 		self.flags.ignore_submit_comment = True
