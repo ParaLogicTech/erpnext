b36355d464d9f8d13cde7865bc3c0a3d7abf8f39
kunhimohamed <kunhimohamed6@gmail.com>
2026-09-25 10:22:41 +0400

    fix[Asset]: change button name from accounting ledger to general ledger

--- erpnext/assets/doctype/asset/asset.js
+++ erpnext/assets/doctype/asset/asset.js
@@ -100,7 +100,7 @@ frappe.ui.form.on('Asset', {
 			}
 
 			if (frm.doc.purchase_receipt || !frm.doc.is_existing_asset) {
-				frm.add_custom_button(__("Accounting Ledger"), function() {
+				frm.add_custom_button(__("General Ledger"), function() {
 					frappe.route_options = {
 						"voucher_no": frm.doc.name,
 						"from_date": frm.doc.available_for_use_date,
